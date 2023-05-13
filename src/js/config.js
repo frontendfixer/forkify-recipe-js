7c2fdbaf380d4d00d56c686080cef0d5c9da24ae
Lakshmikanta <frontendfixer@github.com>
2023-05-13 19:53:04 +0530

    config file for application parameters

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+// https://forkify-api.herokuapp.com/v2
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const API_KEY = '4cdb4abc-94bd-43a3-8bca-cd35a8c2dfea';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
